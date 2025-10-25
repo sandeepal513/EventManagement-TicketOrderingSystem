8f6201bb9587adf3178a15518570590d16e1b7ec
Maheesha22 <maheeshaecc18@gmail.com>
2025-10-25 19:50:08 +0530

    update filter by date

--- php/event_list_php.php
+++ php/event_list_php.php
@@ -13,12 +13,32 @@ if ($conn->connect_error) {
 // Get category filter
 $category = isset($_GET['category']) ? $_GET['category'] : '';
 $sort = isset($_GET['sort']) ? $_GET['sort'] : '';
+$date = isset($_GET['date']) ? $_GET['date'] : '';
+$location = isset($_GET['location']) ? $_GET['location'] : '';
+
 
 // SQL query
+//if ($category) {
+   // $sql = "SELECT * FROM events WHERE category = '" . $conn->real_escape_string($category) . "'";
+//} else {
+   // $sql = "SELECT * FROM events";
+//}
+
+$sql = "SELECT * FROM events";
+$conditions = [];
+
 if ($category) {
-    $sql = "SELECT * FROM events WHERE category = '" . $conn->real_escape_string($category) . "'";
-} else {
-    $sql = "SELECT * FROM events";
+    $conditions[] = "category = '" . $conn->real_escape_string($category) . "'";
+}
+if ($date) {
+    $conditions[] = "date = '" . $conn->real_escape_string($date) . "'";
+}
+if ($location) {
+    $conditions[] = "location LIKE '%" . $conn->real_escape_string($location) . "%'";
+}
+
+if (!empty($conditions)) {
+    $sql .= " WHERE " . implode(" AND ", $conditions);
 }
 
 if ($sort === 'a-z') {
